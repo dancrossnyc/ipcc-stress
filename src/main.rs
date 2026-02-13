99ae0b9e719570fcfdd84d4c5b90e9bd0c669251
Dan Cross <cross@oxidecomputer.com>
2026-02-13 04:15:27 +0000

    simplify reproduction
    Make the reproduction program significantly
    simpler, and introduce variability in the delay
    before we signal the worker thread.
    
    Signed-off-by: Dan Cross <cross@oxidecomputer.com>

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,59 @@
-use attest_data::messages::HostToRotCommand;
-use attest_data::messages::RotToHost;
-use libipcc::IpccError;
-use slog_error_chain::InlineErrorChain;
-use libipcc::IpccHandle;
-use libipcc::ffi::IPCC_MAX_DATA_SIZE;
-use signal_hook::consts::SIGHUP;
-use signal_hook::iterator::Signals;
-use std::sync::atomic::AtomicUsize;
-use std::sync::atomic::Ordering;
-use std::thread;
+use nix::sys::signal::SIGHUP;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
-static NUM_SIGHUP: AtomicUsize = AtomicUsize::new(0);
+static NUMSIG: AtomicUsize = AtomicUsize::new(0);
+
+fn initsighup() {
+    extern "C" fn onhup(_: std::ffi::c_int) {
+        NUMSIG.fetch_add(1, Ordering::Relaxed);
+    }
+
+    use nix::sys::signal::{SaFlags, SigAction, SigHandler, SigSet, sigaction};
+
+    let sighup = SigHandler::Handler(onhup);
+    let sa = SigAction::new(sighup, SaFlags::empty(), SigSet::empty());
+    unsafe {
+        sigaction(SIGHUP, &sa).expect("installed SIGHUP handler");
+    }
+}
+
+fn threadrot() {
+    use attest_data::messages::{self, HostToRotCommand};
+
+    let handle = libipcc::IpccHandle::new().expect("Got IPCC handle");
+    let mut request = [0; attest_data::messages::MAX_REQUEST_SIZE];
+    let len = messages::serialize(&mut request, &HostToRotCommand::GetTqCertificates, |_| 0)
+        .expect("Serialized GetTqCertificates command");
+    loop {
+        let mut response = [0; libipcc::ffi::IPCC_MAX_DATA_SIZE];
+        let _ = handle.rot_request(&request[..len], &mut response[..]);
+    }
+}
+
+fn killthr<T>(id: &JoinHandle<T>) {
+    use std::os::unix::thread::JoinHandleExt;
+    let ptid = id.as_pthread_t();
+    let _r = nix::sys::pthread::pthread_kill(ptid, SIGHUP);
+}
 
 fn main() {
-    let expected = getcert1().expect("got rot certs");
+    initsighup();
 
-    let signals = Signals::new([SIGHUP]).expect("created Signals");
-    thread::spawn(|| {
-        block_sighup_on_current_thread();
-        monitor_incoming_signals();
-    });
-    thread::spawn(|| {
-        block_sighup_on_current_thread();
-        count_incoming_signals(signals);
-    });
-    thread::spawn(|| {
-        block_sighup_on_current_thread();
-        sighup_ourself_a_bunch();
-    });
-
-    loop {
-        match getcert1() {
-            Ok(data) => assert_eq!(data, expected),
-            Err(err) => {
-                println!("IPCC request failed: {}", InlineErrorChain::new(&err));
-            },
-        }
-    }
-}
-
-fn block_sighup_on_current_thread() {
-    use nix::sys::signal::{SigSet, SigmaskHow, Signal, pthread_sigmask};
-
-    let mut sigset = SigSet::empty();
-    sigset.add(Signal::SIGHUP);
-    pthread_sigmask(SigmaskHow::SIG_BLOCK, Some(&sigset), None).expect("blocked SIGHUP");
-}
-
-fn sighup_ourself_a_bunch() {
-    use nix::sys::signal::{Signal, kill};
-    use nix::unistd::getpid;
-
-    loop {
-        kill(getpid(), Signal::SIGHUP).expect("failed to send SIGHUP");
-        thread::sleep(Duration::from_millis(1));
-    }
-}
-
-fn count_incoming_signals(mut signals: Signals) {
-    loop {
-        for signal in signals.pending() {
-            match signal {
-                SIGHUP => {
-                    NUM_SIGHUP.fetch_add(1, Ordering::Relaxed);
-                }
-                _ => {
-                    unreachable!("unexpected signal: {signal}");
-                }
+    let ct = thread::spawn(threadrot);
+    let mut k = 0_usize;
+    let seq = [1, 2, 3, 40, 3, 2, 10, 20];
+    for &delay in seq.iter().cycle() {
+        for _ in 0..delay {
+            thread::sleep(Duration::from_millis(1));
+            k = k.wrapping_add(1);
+            if k.is_multiple_of(5000) {
+                let num = NUMSIG.load(Ordering::Relaxed);
+                println!("iteration {k} signals received: {num}");
             }
         }
+        killthr(&ct);
     }
-}
-
-fn monitor_incoming_signals() {
-    loop {
-        let num = NUM_SIGHUP.load(Ordering::Relaxed);
-        println!("sighups received: {num}");
-        thread::sleep(Duration::from_secs(5));
-    }
-}
-
-fn getcert1() -> Result<Vec<u8>, IpccError> {
-    let handle = IpccHandle::new().unwrap();
-
-    let mut rot_message = vec![0; attest_data::messages::MAX_REQUEST_SIZE];
-    let mut rot_resp = vec![0; IPCC_MAX_DATA_SIZE];
-    let len = attest_data::messages::serialize(
-        &mut rot_message,
-        &HostToRotCommand::GetTqCertificates,
-        |_| 0,
-    )
-    .unwrap();
-    let len = handle.rot_request(&rot_message[..len], &mut rot_resp)?;
-    let data =
-        attest_data::messages::parse_response(&rot_resp[..len], RotToHost::RotTqCertificates)
-            .unwrap();
-    Ok(data.to_vec())
 }
